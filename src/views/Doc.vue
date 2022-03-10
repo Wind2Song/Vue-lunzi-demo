35169cafb7de0dd9f7b9b84eb99d29fc071ee1a5
MadarainCong <huangcong0307@163.com>
2022-03-10 09:41:17 +0800

    Doc aside 样式添加

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
     <div>
         <Topnav />
         <div class="content">
-            <aside>边栏</aside>
+            <aside>
+                <h1>组件列表</h1>
+                <ol>
+                    <li>
+                        <router-link to="/Doc/switch">Switch</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/Doc/button">Button</router-link> 
+                    </li>
+                    <li>
+                        <router-link to="/Doc/dialog">Dialog</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/Doc/>tabs">Tabs</router-link>
+                    </li>
+                </ol>
+            </aside>
             <main>主内容</main>
         </div>        
     </div>
@@ -13,3 +29,21 @@ export default {
     components:{Topnav}
 }
 </script>
+<style lang="scss" scoped>
+aside{
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
