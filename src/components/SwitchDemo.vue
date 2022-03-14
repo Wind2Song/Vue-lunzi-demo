786d68b74744b2f2a55823931425f4f8319f3752
MadarainCong <huangcong0307@163.com>
2022-03-14 13:07:05 +0800

    开关悬浮右滚

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <Switch />
+        <h2>功能说明</h2>
+        <p>
+            Switch value="true" // 字符串true 表示“开"
+            Switch:value="true" // 布尔值true 表示“开"
+        </p>
     </div>
 </template>
 <script lang="ts">
