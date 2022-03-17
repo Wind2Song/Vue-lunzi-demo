30cbfd14085862c9ea19e7f3a56460e12405a85e
MadarainCong <huangcong0307@163.com>
2022-03-17 21:30:34 +0800

    支持level属性

--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -9,14 +9,16 @@ import { computed } from 'vue';
 export default{
   props:{
     theme:{type:String,default:'button'},
-    size:{type:String,default:'normal'}
+    size:{type:String,default:'normal'},
+    level:{type:String,default:'normal'}
   },
   setup(props){
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(()=>{ 
       return {
         [`lunzi-theme-${theme}`]: theme,
-        [`lunzi-size-${size}`]: size
+        [`lunzi-size-${size}`]: size,
+        [`lunzi-level-${level}`]: level,
       }
     })
     return {classes}
@@ -29,6 +31,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .lunzi-button {
   box-sizing: border-box;
   height: $h;
@@ -43,6 +46,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -82,6 +86,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.lunzi-theme-button {
+    &.lunzi-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.lunzi-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.lunzi-theme-link {
+    &.lunzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.lunzi-theme-text {
+    &.lunzi-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.lunzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 
