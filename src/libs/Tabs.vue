9ce56b7712d1e4aca91f0d3c05c148ada4904f78
MadarainCong <huangcong0307@163.com>
2022-03-29 15:58:02 +0800

    获取横线div 宽度

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,30 +1,68 @@
 <template>
-    <div class="lunzi-tabs">
-        <div class="lunzi-tabs-nav">
-            <div class="lunzi-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
-        </div>
-        <div class="lunzi-tabs-content">
-          <component class="lunzi-tabs-content-item" 
-          v-for="(c,index) in defaults" :is="c" :key="index" />
-        </div>
+  <div class="lunzi-tabs">
+    <div class="lunzi-tabs-nav">
+      <div class="lunzi-tabs-nav-item" v-for="(t,i) in titles" 
+      :ref="(el) => { if (el) navItems[i] = el }" 
+      @click="select(t)" :class="{selected: t=== selected}" :key="i" 
+      >{{t}}</div>
+      <div class="lunzi-tabs-nav-indicator" ref="indicator"></div>
     </div>
+    <div class="lunzi-tabs-content">
+      <component class="lunzi-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="(c,index) in defaults" :is="c" key="index" />
+
+    </div>
+  </div>
 </template>
+
 <script lang="ts">
 import Tab from './Tab.vue'
+import {
+  computed, onMounted, ref
+} from 'vue'
 export default {
-    setup(props,context){
-        const defaults = context.slots.default() 
-        // console.log({...context.slots.default()})
-        defaults.forEach((tag)=>{
-            if(tag.type !== Tab){
-                throw new Error('子标签必须是Tab')
-            }
-        })
-        const titles = defaults.map((tag)=>{
-            return tag.props.title
-        })
-        return {defaults,titles}
+  props: {
+    selected: {
+      type: String
     }
+  },
+  setup(props, context) {
+    const navItems = ref < HTMLDivElement[] > ([])
+    const indicator = ref <HTMLDivElement>(null)
+    onMounted(()=>{
+      const divs = navItems.value
+      // console.log(navItems.value)
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      console.log(result)
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+    })
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+    const current = computed(() => {
+      console.log('重新 return')
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -36,6 +74,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -47,9 +86,23 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+    &-indicator{
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
   &-content {
     padding: 8px 0;
+    &-item{
+      display: none;
+      &.selected{
+        display: block;
+      }
+    }
   }
 }
 </style>
